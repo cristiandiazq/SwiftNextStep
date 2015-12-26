9d6bb1935d5e17b67e3a4ac1b4fe5addbb3e4d51
Cristian Diaz <cristiandiazq@icloud.com>
2015-12-26 19:43:36 -0400

    Initial Commit

---
+++ SwiftNextStep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNextStep
+//
+//  Created by Cristian Diaz on 12/26/15.
+//  Copyright © 2015 MyApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
